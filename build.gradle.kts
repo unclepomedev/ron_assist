714be0f05591aaa3727860647df5ae28397f084f
Pome <unclepome.dev@gmail.com>
2026-05-10 16:51:41 +0900

    init and clean up

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,63 @@
 import org.jetbrains.intellij.platform.gradle.TestFrameworkType
+import org.jetbrains.grammarkit.tasks.GenerateLexerTask
+import org.jetbrains.grammarkit.tasks.GenerateParserTask
 
 plugins {
     id("org.jetbrains.kotlin.jvm")
     id("org.jetbrains.intellij.platform")
     id("org.jetbrains.changelog")
+    id("org.jetbrains.grammarkit") version "2023.3.0.3"
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+repositories {
+    mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
 }
 
 dependencies {
     testImplementation("junit:junit:4.13.2")
 
-    // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
-        intellijIdea("2025.2.6.2")
+        intellijIdea("2026.1")
+        plugin("com.redhat.devtools.lsp4ij:0.19.3")
         testFramework(TestFrameworkType.Platform)
+    }
+}
+
+intellijPlatform {
+    pluginConfiguration {
+        name = "RON Assist"
+    }
+}
+
+sourceSets {
+    main {
+        java.srcDirs("src/main/gen")
+    }
+}
+
+tasks {
+    named<GenerateLexerTask>("generateLexer") {
+        sourceFile.set(file("src/main/grammar/Ron.flex"))
+        targetOutputDir.set(file("src/main/gen/com/github/unclepomedev/ronassist/lexer"))
+        purgeOldFiles.set(true)
+    }
+
+    named<GenerateParserTask>("generateParser") {
+        sourceFile.set(file("src/main/grammar/Ron.bnf"))
+        targetRootOutputDir.set(file("src/main/gen"))
+        pathToParser.set("/com/github/unclepomedev/ronassist/parser/RonParser.java")
+        pathToPsiRoot.set("/com/github/unclepomedev/ronassist/psi")
+        purgeOldFiles.set(true)
+    }
+
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        dependsOn("generateLexer", "generateParser")
     }
 }
