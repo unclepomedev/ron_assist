de4424c1e970f5370a81e69c2978b189a0606b40
Pome <unclepome.dev@gmail.com>
2026-05-10 19:29:57 +0900

    add icon

--- src/main/kotlin/com/github/unclepomedev/ronassist/lang/RonFileType.kt
+++ src/main/kotlin/com/github/unclepomedev/ronassist/lang/RonFileType.kt
@@ -1,13 +1,14 @@
 package com.github.unclepomedev.ronassist.lang
 
 import com.intellij.openapi.fileTypes.LanguageFileType
+import com.intellij.openapi.util.IconLoader
 import javax.swing.Icon
 
 class RonFileType private constructor() : LanguageFileType(RonLanguage.INSTANCE) {
     override fun getName(): String = "RON"
     override fun getDescription(): String = "Rust object notation"
     override fun getDefaultExtension(): String = "ron"
-    override fun getIcon(): Icon? = null
+    override fun getIcon(): Icon = IconLoader.getIcon("/icons/ron.svg", RonFileType::class.java)
 
     companion object {
         val INSTANCE = RonFileType()
