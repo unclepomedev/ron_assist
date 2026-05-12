fe6aa157092874fa10574697bbcbd5d097f0af95
Pome <unclepome.dev@gmail.com>
2026-05-12 22:07:15 +0900

    feat: Implement 'New RON File' action with basic template

---
+++ src/main/kotlin/com/github/unclepomedev/ronassist/actions/CreateRonFileAction.kt
@@ -0,0 +1,26 @@
+package com.github.unclepomedev.ronassist.actions
+
+import com.github.unclepomedev.ronassist.icons.RonIcons
+import com.intellij.ide.actions.CreateFileFromTemplateAction
+import com.intellij.ide.actions.CreateFileFromTemplateDialog
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiDirectory
+
+class CreateRonFileAction : CreateFileFromTemplateAction(
+    "RON File",
+    "Create a new RON file",
+    RonIcons.FILE,
+) {
+    override fun buildDialog(
+        project: Project,
+        directory: PsiDirectory,
+        builder: CreateFileFromTemplateDialog.Builder,
+    ) {
+        builder
+            .setTitle("New RON File")
+            .addKind("Empty RON file", RonIcons.FILE, "RON File")
+    }
+
+    override fun getActionName(directory: PsiDirectory, newName: String, templateName: String): String =
+        "Create RON File"
+}
